917086a2db425cbd8a9b0c20d4d01d271adc3547
adcor <adironc@gmail.com>
2016-04-06 18:34:24 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+<html>
+  <head></head>
+  <body>
+    <div id="game"></div>
+    <script type="text/javascript" src="https://rawgithub.com/craftyjs/Crafty/release/dist/crafty-min.js"></script>
+    <script>
+      Crafty.init(500,350, document.getElementById('game'));
+    </script>
+  </body>
+</html>
